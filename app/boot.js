c5866531fa914a5caaa3aa8cb11c327f06bca563
advgal <peter.reif@wien.gv.at>
2016-04-19 09:18:43 +0200

    added production mode

--- app/boot.js
+++ app/boot.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './root.component', "angular2/router"], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './root.component', "angular2/router", "angular2/core"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, root_component_1, router_1;
+    var browser_1, root_component_1, router_1, core_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -12,8 +12,12 @@ System.register(['angular2/platform/browser', './root.component', "angular2/rout
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             browser_1.bootstrap(root_component_1.RootComponent, [router_1.ROUTER_PROVIDERS]);
         }
     }
